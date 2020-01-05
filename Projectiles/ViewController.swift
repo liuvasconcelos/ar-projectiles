bb02ccd0acd453393f7de5b0c70f08bc689b6077
liuvasconcelos <livia.vasconcelos@stant.com.br>
2020-01-05 19:55:57 -0300

    develop Add ball
    -
    
    Signed-off-by: liuvasconcelos <livia.vasconcelos@stant.com.br>

--- Projectiles/ViewController.swift
+++ Projectiles/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet weak var planeDetectedLabel: UILabel!
     
     let configuration = ARWorldTrackingConfiguration()
+    var basketAdded: Bool {
+        return sceneView.scene.rootNode.childNode(withName: "Basket", recursively: false) != nil
+    }
+    var power: Float = 1.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,7 +53,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let zPosition = positionOfPlane.z
         
         basketNode?.position = SCNVector3(xPosition, yPosition, zPosition)
-        
+        basketNode?.physicsBody = SCNPhysicsBody(type: .static,
+                                                 shape: SCNPhysicsShape(node: basketNode ?? SCNNode(),
+                                                                        options: [SCNPhysicsShape.Option.keepAsCompound : true,
+                                                                             SCNPhysicsShape.Option.type: SCNPhysicsShape.ShapeType.concavePolyhedron]))
         if let basketNode = basketNode {
             self.sceneView.scene.rootNode.addChildNode(basketNode)
         }
@@ -71,5 +78,31 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if self.basketAdded {
+            guard let pointOfView = self.sceneView.pointOfView else { return }
+            self.power = 10
+            let transform = pointOfView.transform
+            let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+            let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
+            let position = location + orientation
+            
+            let ball = SCNNode(geometry: SCNSphere(radius: 0.3))
+            ball.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "ball")
+            ball.position = position
+            
+            let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: ball))
+            ball.physicsBody = body
+            ball.physicsBody?.applyForce(SCNVector3(orientation.x*power,
+                                                    orientation.y*power,
+                                                    orientation.z*power),
+                                         asImpulse: true)
+            sceneView.scene.rootNode.addChildNode(ball)
+        }
+    }
+    
 }
 
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+}
