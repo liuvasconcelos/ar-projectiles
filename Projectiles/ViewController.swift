cd8f8e3c5f42324f211de44ee9c2430febd8b54b
liuvasconcelos <livia.vasconcelos@stant.com.br>
2020-01-05 19:25:41 -0300

    develop Add basketball scenerio.
    -
    
    Signed-off-by: liuvasconcelos <livia.vasconcelos@stant.com.br>

--- Projectiles/ViewController.swift
+++ Projectiles/ViewController.swift
@@ -7,14 +7,69 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var planeDetectedLabel: UILabel!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configuration.planeDetection = .horizontal
+        sceneView.debugOptions = [SCNDebugOptions.showWorldOrigin, SCNDebugOptions.showFeaturePoints]
+        sceneView.session.run(configuration)
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.delegate = self
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        sceneView.addGestureRecognizer(tap)
     }
 
-
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        guard let sceneView = sender.view as? ARSCNView else { return }
+        let location = sender.location(in: sceneView)
+        let hitTestResult = sceneView.hitTest(location, types: .existingPlaneUsingExtent)
+        
+        if let hitTest = hitTestResult.first {
+            self.addBasket(hitTestResult: hitTest)
+        }
+    }
+    
+    func addBasket(hitTestResult: ARHitTestResult) {
+        let basketScene = SCNScene(named: "Basketball.scnassets/Basketball.scn")
+        let basketNode  = basketScene?.rootNode.childNode(withName: "Basket", recursively: false)
+        let positionOfPlane = hitTestResult.worldTransform.columns.3
+        
+        let xPosition = positionOfPlane.x
+        let yPosition = positionOfPlane.y
+        let zPosition = positionOfPlane.z
+        
+        basketNode?.position = SCNVector3(xPosition, yPosition, zPosition)
+        
+        if let basketNode = basketNode {
+            self.sceneView.scene.rootNode.addChildNode(basketNode)
+        }
+        
+    }
+    
+    @IBAction func addButtonClicked(_ sender: Any) {
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else { return }
+        DispatchQueue.main.async {
+            self.planeDetectedLabel.isHidden = false
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.planeDetectedLabel.isHidden = true
+        }
+        
+    }
+    
 }
 
